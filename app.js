9dfeb42a6c3727d507e33fead6a55dfaa636452e
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-04-29 23:27:12 -0300

    criando a navbar
    criando a navbar

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('hbs');  // importar o hbs
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -12,6 +13,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+// registra partials do handlebars (views/partials)
+hbs.registerPartials(path.join( __dirname, 'views', 'partials'));
+//
 
 app.use(logger('dev'));
 app.use(express.json());
